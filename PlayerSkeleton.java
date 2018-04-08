9d8b5b8dda5fc22fa23ce3e41079818c01334a5b
Kevin Leonardo A <k.l.a@u.nus.edu>
2018-04-08 20:32:56 +0800

    Change evaluation to use the added methods

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -40,6 +40,10 @@ public class PlayerSkeleton {
 		valuation += s.cleared * weights[0];
 		valuation += getHighestCol(s.field) * weights[1];
 		valuation += getLowestCol(s.field) * weights[2];
+		valuation +=  getHolesCount(s.field, s.tops) * weights[3];
+		valuation += getBlockadeCount(s.field, s.tops) * weights[4];
+		valuation += getParityCount(s.field) * weights[5] ;
+		valuation += getTotalHeightDiff(s.tops) * weights[6];
 
 		return valuation;
 	}
